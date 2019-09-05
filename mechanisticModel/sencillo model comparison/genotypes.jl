97adf9273e3cf4a373da7cb5c9b76f268dbfd90e
msturroc <marcsturrock@gmail.com>
2019-09-05 20:07:18 +0100

    changing functions for compatibility with ABC

--- mechanisticModel/sencillo model comparison/genotypes.jl
+++ mechanisticModel/sencillo model comparison/genotypes.jl
@@ -3,42 +3,44 @@
 function wt(params)
 	params
 end
-	
+ 
 function mth1ko(params)
-	pars=OrderedDict(params)
-	pars[:smth1]=0.0
-	pars[:Mth1_0]=0.0
-	collect(pars)
+ pars = copy(params)
+	pars[13] = log(0.0)
+	pars[47] = log(0.0)
+	return collect(pars)
 end
-	
+ 
 function std1ko(params)
-	pars=OrderedDict(params)
-	pars[:std1tot]=0.0
-	pars[:Std1_0]=0.0
-	collect(pars)
-end	
-function mig2ko(params)
-	pars=OrderedDict(params)
-	pars[:smig2]=0.0
-	pars[:Mig2_0]=0.0
-	collect(pars)
-end	
-function mig1ko(params)
-	pars=OrderedDict(params)
-	pars[:mig1tot]=0.0
-	pars[:Mig1_0]=0.0
-	collect(pars)
-end	
-function rgt2ko(params)
-	pars=OrderedDict(params)
-	pars[:dmth1rgt2] = 0.0
-	pars[:dstd1rgt2] = 0.0
-	collect(pars)
-end	
-function snf3ko(params)
-	pars=OrderedDict(params)
-	pars[:dmth1snf3] = 0.0
-	pars[:dstdtsnf3] = 0.0
-	collect(pars)
+ pars = copy(params)
+	pars[18] = log(0.0)
+	pars[48] = log(0.0)
+	return collect(pars)
 end	
 
+function mig2ko(params)
+ pars = copy(params)
+	pars[29] = log(0.0)
+ pars[46] = log(0.0)
+ return collect(pars)
+end	
+
+function mig1ko(params)
+ pars = copy(params)
+	pars[22] = log(0.0)
+ pars[45] = log(0.0)
+ return collect(pars)
+end	
+
+function rgt2ko(params)
+ pars = copy(params)
+ pars[12] = log(0.0)
+ pars[2] = log(100.0) #should this be another parameter?
+ return collect(pars)
+end	
+
+function snf3ko(params)
+ pars = copy(params)
+ pars[11] = log(0.0)
+ return collect(pars)
+end
